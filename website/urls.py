19cdf1bee31f5ceea16237ac96e424b0257ae071
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-06-08 13:44:29 +0600

    initial commit

---
+++ website/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.urls.conf import include
+from . import views
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('contact.html',views.contact,'contact')
+]
